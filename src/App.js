ee30a2a680e3f0f3878ffc00fe9aa20aa8f87eae
Austin Atmaja <austin.atmaja@gmail.com>
2018-09-15 10:11:47 -0400

    added basic routing and application structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
 import React, { Component } from 'react';
 import './App.css';
 import GoogleLogin from 'react-google-login';
+import { Link, Switch, Route } from 'react-router-dom'
+import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import Home from './Home'
+import Profile from './Profile'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoggedIn: false
+    };
+    this.responseGoogle = this.responseGoogle.bind(this);
+  }
+
   render() {
     return (
-      <GoogleLogin
-        clientId="671198457660-d5gotcj5u380n416mtgcrj44364affs0.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={this.responseGoogle}
-        onFailure={this.responseGoogle}
-      />
+      <div>
+        {!this.state.isLoggedIn && (
+          <GoogleLogin
+            clientId="671198457660-d5gotcj5u380n416mtgcrj44364affs0.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={this.responseGoogle}
+            onFailure={this.responseGoogle}>
+          </GoogleLogin>
+        )}
+        {this.state.isLoggedIn && (
+          <div>
+            <Navbar fluid collapseOnSelect>
+              <Navbar.Header>
+                <Navbar.Brand>
+                  <Link to="/" exact>You Pick</Link>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+              </Navbar.Header>
+              <Navbar.Collapse>
+                <Nav pullRight>
+                <LinkContainer to="/" exact>
+                  <NavItem>Home</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/Profile">
+                  <NavItem>Profile</NavItem>
+                </LinkContainer>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+            <Switch>
+              <Route exact path="/" component={Home}></Route>
+              <Route path="/Profile" component={Profile}></Route>
+            </Switch>
+          </div>
+
+        )}
+      </div>
+
     );
   }
 
   responseGoogle(response){
-    console.log(response);
+    this.setState({isLoggedIn: true});
+    console.log(this.state);
   }
 }
 
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>I'm home</div>
+    );
+  }
+}
+
+export default Home;
