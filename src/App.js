1217e945d391355841fd7e5f3976b3f5108d3d23
Austin Atmaja <austin.atmaja@gmail.com>
2018-09-15 04:58:12 -0400

    added google sign in

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import GoogleLogin from 'react-google-login';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <GoogleLogin
+        clientId="671198457660-d5gotcj5u380n416mtgcrj44364affs0.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={this.responseGoogle}
+        onFailure={this.responseGoogle}
+      />
     );
+  }
+
+  responseGoogle(response){
+    console.log(response);
   }
 }
 
