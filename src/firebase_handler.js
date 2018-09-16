83fa0b19fdce131dba37289929c65397fdd3787d
Austin Atmaja <austin.atmaja@gmail.com>
2018-09-16 02:45:11 -0400

    update instead of set when child doesn't exist

--- src/firebase_handler.js
+++ src/firebase_handler.js
@@ -2,16 +2,21 @@ class FirebaseHandler{
   constructor(){
       this.database = window.firebase.database();
   }
-  writeTest() {
-    this.database.ref('testUser').set({
-      username: "testName",
-      email: "testEmail"
-    });
-  }
   writeData(id, latitude, longitude) {
-    this.database.ref(id).set({
-      lat: latitude,
-      lng: longitude
+    this.database.ref(id).once("value").then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log("exists"); 
+        this.database.ref(id).update({
+          lat: latitude,
+          lng: longitude,
+        });
+      } else {
+        console.log("doesn't exist"); 
+        this.database.ref(id).set({
+          lat: latitude,
+          lng: longitude
+        });
+      }
     });
   }
 }
